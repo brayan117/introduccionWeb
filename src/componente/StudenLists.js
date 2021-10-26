2ec65ecf6c63f103c49784198c66a350004baf6f
brayan <duranbdj12@gmail.com>
2021-10-26 15:39:58 -0500

    modal realizado

--- src/componente/StudenLists.js
+++ src/componente/StudenLists.js
@@ -1,4 +1,4 @@
-import React from "react";
+import React, {useState} from "react";
 import StudenItem from "./StudenItem";
 import Row from 'react-bootstrap/Row'
 import Col from 'react-bootstrap/Col'
@@ -6,17 +6,25 @@ import StudentForm from "./StudentForm";
 
 const StudenLists =()=>{
 
-    const students =[{nombre: "alvaro", programa: "ingenieria de sistemas", semestre: 4}
+    const studentsIniciales =[{nombre: "alvaro", programa: "ingenieria de sistemas", semestre: 4}
                     ,{nombre: "jose", programa: "ingenieria de sistemas", semestre: 5},
                     {nombre: "luis", programa: "ingenieria de sistemas", semestre: 6},
                     {nombre: "camilo", programa: "ingenieria de sistemas", semestre: 8},
                     {nombre: "liz", programa: "ingenieria de sistemas", semestre: 1}
                 
                 ]
+
+     const [students, setStudents]= useState(studentsIniciales);
+
+    const alEnviarForm = (studentFromForm)=>{
+     
+        setStudents([...students,studentFromForm]);
+
+    }
     return(
         <>
         <Row>
-             <StudentForm/>
+             <StudentForm agregar ={alEnviarForm}/>
         </Row>
 
           <Row className ="border">
--- src/componente/StudentForm.js
+++ src/componente/StudentForm.js
@@ -4,12 +4,31 @@ import 'bootstrap/dist/css/bootstrap.min.css';
 import Button from 'react-bootstrap/Button';
 
 
-export default function StudentForm (){
-    
-        const [show, setShow] = useState(false);
+export default function StudentForm (props){
+  
+  const [show, setShow] = useState(false);
       
-        const handleClose = () => setShow(false);
-        const handleShow = () => setShow(true);
+  const handleClose = () => setShow(false);
+  const handleShow = () => setShow(true);
+
+  const estadoInicial =
+{
+  nombre:"", programa:"",semestre:""
+}    
+
+const [estadoForm, cambiarEstadoForm] = useState(estadoInicial);
+
+const gestionarCamposForm = e =>{
+    const {name, value}= e.target;
+    cambiarEstadoForm ({...estadoForm,[name]:value});
+}
+
+const onSubmitForm = ev =>{
+   ev.preventDefault();
+   props.agregar(estadoForm);
+    
+}
+
       
         return (
           <>
@@ -17,30 +36,30 @@ export default function StudentForm (){
               Launch demo modal
             </Button>
       
-            <Modal show={show} onHide={handleClose}>
+            <Modal show={show} onHide={handleClose}   onSubmit={onSubmitForm}>
               <Modal.Header closeButton>
                 <Modal.Title>Modal heading</Modal.Title>
               </Modal.Header>
-              <Modal.Body><form>
+              <Modal.Body><form >
               <div className="mb-3">
-              <label htmlFor ="nameStudent" className="form-label">Name</label>
-              <input type="text" className="form-control" id="nameStudent"/>
+              <label htmlFor ="nameStudent" className="form-label"  >Name</label>
+              <input type="text" name ="nombre" className="form-control" id="nameStudent" value={estadoForm.nombre} onChange={gestionarCamposForm} />
               </div>
               <div className="mb-3">
               <label htmlFor ="program" className="form-label">Programa</label>
-              <input type="text" className="form-control" id="program"/>
+              <input type="text" name="programa" className="form-control" id="program" value={estadoForm.programa} onChange={gestionarCamposForm}/>
               </div>
               <div classNamea="mb-3">
               <label htmlFor ="grade" className="form-label">semestre</label>
-              <input type="text" className="form-control" id="grade"/>
+              <input type="text" name="semestre" className="form-control" id="grade" value={estadoForm.semestre} onChange={gestionarCamposForm}/>
               </div>
               
-          </form></Modal.Body>
+          </form></Modal.Body >
               <Modal.Footer>
                 <Button variant="secondary" onClick={handleClose}>
                   Close
                 </Button>
-                <Button variant="primary" onClick={handleClose}>
+                <Button variant="primary" type="submit" onClick={onSubmitForm}>
                   Agregar
                 </Button>
               </Modal.Footer>
